89843a0a63e24ef32f18b69f537526b3d6529ceb
seonongs <seonongs@gmail.com>
2022-06-03 10:39:39 +0900

    changed

--- src/main/java/io/beneta/board/model/Board.java
+++ src/main/java/io/beneta/board/model/Board.java
@@ -2,13 +2,11 @@ package io.beneta.board.model;
 
 import io.beneta.common.BaseTimeEntity;
 import lombok.*;
-import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -34,16 +32,4 @@ public class Board extends BaseTimeEntity {
         return this;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Board board = (Board) o;
-        return id != null && Objects.equals(id, board.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
